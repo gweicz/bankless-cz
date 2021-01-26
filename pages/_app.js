f4c18cc801a40faf3d78f776527ad75e78e6e487
Rostislav Křemeček <rostislav.kremecek@ictmedia.cz>
2021-01-26 15:42:59 +0100

    add icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,20 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab, faFacebookF, faTwitter, faInstagram, faDiscord } from '@fortawesome/free-brands-svg-icons'
-import { faSearch, faBars, faAngleUp, faLink } from '@fortawesome/free-solid-svg-icons'
+import { fab, faFacebookF, faTwitter, faInstagram, faDiscord, faReddit } from '@fortawesome/free-brands-svg-icons'
+import { faSearch, faBars, faAngleUp, faLink, faGlobe } from '@fortawesome/free-solid-svg-icons'
 
 import Header from "components/Layout/Header";
 import Footer from "components/Layout/Footer";
 import BackToTop from "components/Layout/BackToTop";
 
-import 'bootstrap/scss/bootstrap.scss'
+import 'bootstrap/scss/bootstrap.scss';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 
 import 'styles/globals.scss'
 
 
 library.add(
-  faSearch, faBars, faLink, faAngleUp,
-  fab, faFacebookF, faTwitter, faInstagram, faDiscord
+  faSearch, faBars, faLink, faAngleUp, faGlobe,
+  fab, faFacebookF, faTwitter, faInstagram, faDiscord, faReddit
 );
 
 function MyApp({ Component, pageProps }) {
