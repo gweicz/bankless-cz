2673f22038904fe8781ae23d212526536b2ba88b
rixcian <rostislav.kremecek@protonmail.com>
2021-04-20 22:13:31 +0200

    refactor: update social links

--- components/SocInvite.tsx
+++ components/SocInvite.tsx
@@ -3,20 +3,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const SocInvite = () => {
   return (
     <ul className="social-icon md-size justify-content-center">
-      {/* <li><a href="#"><FontAwesomeIcon icon={["fab", "facebook-f"]}/></a></li> */}
-      {/* <li><a href="#"><FontAwesomeIcon icon={["fab", "instagram"]}/></a></li> */}
       <li>
-        <a href="https://twitter.com/gweicz" target="_blank">
+        <a href="https://twitter.com/banklesscz" target="_blank">
           <FontAwesomeIcon icon={['fab', 'twitter']} />
         </a>
       </li>
       <li>
-        <a href="https://discord.gg/CYUDRVRt" target="_blank">
+        <a href="https://discord.com/invite/3SrAtJ2gd3" target="_blank">
           <FontAwesomeIcon icon={['fab', 'discord']} />
         </a>
       </li>
       <li>
-        <a href="https://t.me/bankless_cz" target="_blank">
+        <a href="https://t.me/banklesscz" target="_blank">
           <FontAwesomeIcon icon={['fab', 'telegram']} />
         </a>
       </li>
